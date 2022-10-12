b7ac461dcdf17e1cccc4187215517198829b3400
RA1ZER <ra1zer.art@gmail.com>
2022-10-12 20:16:40 +0300

    Задание 18

--- src/module4/Aircraft.kt
+++ src/module4/Aircraft.kt
@@ -2,10 +2,7 @@ package module4
 
 import kotlin.math.roundToInt
 
-class Aircraft {
-    var number: String = "318-100"
-    var maxFlightRange: Double = 6000.0
-    var tankCapacity: Double = 23860.0
+class Aircraft(var number: String, var maxFlightRange: Double, var tankCapacity: Double) {
     var fuelConsumption: Double = 420.0
         get() = ((tankCapacity / (maxFlightRange / 100)) * 10).roundToInt() / 10.0
 
