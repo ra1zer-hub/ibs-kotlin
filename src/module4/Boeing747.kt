ceb3a0882a39f7ff58fface84a4e911eb117e666
RA1ZER <ra1zer.art@gmail.com>
2022-10-13 20:44:05 +0300

    Задание 20

--- src/module4/Boeing747.kt
+++ src/module4/Boeing747.kt
@@ -1,4 +1,6 @@
 package module4
 
-class Boeing747(val passengerCapacity: Int, number: String, maxFlightRange: Double, tankCapacity: Double) :
-    Aircraft(number, maxFlightRange, tankCapacity)
+class Boeing747(number: String, maxFlightRange: Double, tankCapacity: Double, override val passengerCapacity: Int) :
+    Aircraft(number, maxFlightRange, tankCapacity), Passenger {
+
+    }
---
+++ src/module4/Passenger.kt
@@ -0,0 +1,5 @@
+package module4
+
+interface Passenger {
+    val passengerCapacity: Int
+}
