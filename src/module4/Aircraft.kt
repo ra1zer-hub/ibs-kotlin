510c49e3df34f5f5e5010f5f2a17c0a8f646e738
RA1ZER <ra1zer.art@gmail.com>
2022-10-13 20:42:34 +0300

    Задание 19

--- src/module4/Aircraft.kt
+++ src/module4/Aircraft.kt
@@ -2,7 +2,7 @@ package module4
 
 import kotlin.math.roundToInt
 
-class Aircraft(var number: String, var maxFlightRange: Double, var tankCapacity: Double) {
+open class Aircraft(val number: String, val maxFlightRange: Double, val tankCapacity: Double) {
     var fuelConsumption: Double = 420.0
         get() = ((tankCapacity / (maxFlightRange / 100)) * 10).roundToInt() / 10.0
 
---
+++ src/module4/Boeing747.kt
@@ -0,0 +1,4 @@
+package module4
+
+class Boeing747(val passengerCapacity: Int, number: String, maxFlightRange: Double, tankCapacity: Double) :
+    Aircraft(number, maxFlightRange, tankCapacity)
