6fffe8124c0c8629de8ac8087c34004163ed4f96
RA1ZER <ra1zer.art@gmail.com>
2022-10-14 11:52:39 +0300

    Задание 22

--- src/module4/Aircraft.kt
+++ src/module4/Aircraft.kt
@@ -2,8 +2,12 @@ package module4
 
 import kotlin.math.roundToInt
 
-abstract class Aircraft(val number: String, val maxFlightRange: Double, val tankCapacity: Double) {
-    val fuelConsumption: Double
+abstract class Aircraft(protected val number: String, protected val maxFlightRange: Double, protected val tankCapacity: Double) {
+    protected val fuelConsumption: Double
         get() = ((tankCapacity / (maxFlightRange / 100)) * 10).roundToInt() / 10.0
 
+    open fun info() {
+        println("Номер воздушного судна: $number\nМаксимальная дальность полета: $maxFlightRange\n" +
+                "Вместимость бака: $tankCapacity\nРасход топлива на 100 км: $fuelConsumption")
+    }
 }
--- src/module4/Boeing747.kt
+++ src/module4/Boeing747.kt
@@ -2,5 +2,8 @@ package module4
 
 class Boeing747(number: String, maxFlightRange: Double, tankCapacity: Double, override val passengerCapacity: Int) :
     Aircraft(number, maxFlightRange, tankCapacity), Passenger {
-
+    override fun info() {
+        super.info()
+        println("Вместимость пассажиров: $passengerCapacity")
     }
+}
